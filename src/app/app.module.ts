ea08e3f8f6f8afbcab307eb348e6d2a96d9172d3
nanyanjiang <nanyan.jiang001@umb.edu>
2020-08-20 14:22:28 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from 'src/app/components/home/home.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', pathMatch: 'full', redirectTo: 'home'},
+      {path: 'home', component: HomeComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
